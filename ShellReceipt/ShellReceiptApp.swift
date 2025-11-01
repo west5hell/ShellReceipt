0b4c3808aa4d95cfd29388692b9122406866b450
Ponter Chia <dozeuto@gmail.com>
2025-11-01 17:03:10 +0800

    add project

---
+++ ShellReceipt/ShellReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShellReceiptApp.swift
+//  ShellReceipt
+//
+//  Created by Pongt Chia on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ShellReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
